21340623e91b3575aea1a83f033db44fdbe46771
kwamboka1 <makorimildred@gmail.com>
2021-08-31 06:50:49 -0700

    Create 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -7,14 +7,16 @@
  */
 int main(void)
 {
-    int t;
-
-    t = _islower('H');
-    _putchar(t + '0');
-    t = _islower('o');
-    _putchar(t + '0');
-    t = _islower(108);
-    _putchar(t + '0');
-    _putchar('\n');
-    return (0);
+  int c;
+  
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
 }
+  
