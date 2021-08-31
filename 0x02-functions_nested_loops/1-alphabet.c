4fb69e9fd6677a9c43c8f6e805b826fab9e720ce
kwamboka1 <makorimildred@gmail.com>
2021-08-31 05:31:27 -0700

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,19 @@
 #include "main.h"
 
+
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 
-void print_alphabet(void)
+int main(void)
 {
-int lowera;
+int lowa;
 
-for (lowera = 'a'; lowera <= 'z'; lowera++)
-putchar(lowera);
+for (lowa='a'; lowa<='z'; lowa++)
+
+putchar(lowa);
 putchar('\n');
 
 return (0);
