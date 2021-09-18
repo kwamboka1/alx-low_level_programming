94f7837a79873c066e04e785e103a60832bf60bf
kwamboka1 <makorimildred@gmail.com>
2021-09-18 01:33:40 -0700

    Modified all files to have betty coding style

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,10 +4,13 @@
  * @argc: number of command line arguments.
  * @argv: array that contains the program command line arguments.
  * Return: 0 - success.
- * */
+ **/
+
 int main(int argc __attribute__((unused)), char *argv[])
+
 {
 	printf("%s\n", argv[0]);
+
 	return (0);
 
 }
--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
  * main - multiplies two numbers.
  * @argc: number of command line arguments.
  * @argv: array that contains the program command line arguments.
  * Return: 0 - success.
- */
+ **/
+
 int main(int argc, char *argv[])
+
 {
+
 	if (argc != 3)
 	{
 		printf("Error\n");
