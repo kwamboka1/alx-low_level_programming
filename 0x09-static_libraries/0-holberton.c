15d4cd5c19424f1af4222cc618b0234762de6be1
Kwamboka1 <makorimildred@gmail.com>
2021-09-19 22:11:58 +0300

    adding c files

---
+++ 0x09-static_libraries/0-holberton.c
@@ -0,0 +1,24 @@
+#include"main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	char ch[8] = "_putchar";
+	int i;
+
+	for (i = 0; i <= 8; i++)
+
+	{
+		_putchar(ch[i]);
+
+	}
+	_putchar('\n');
+
+	return (0);
+}
