04212defea6bc959b3882dc7c61783e70316e254
Kwamboka1 <makorimildred@gmail.com>
2021-09-19 21:16:08 +0300

    add all c files

---
+++ 0x09-static_libraries/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers.
+ * @d: first number.
+ * @f: second number.
+ * Return: multiplication.
+ */
+int mul(int d, int f)
+{
+	return (d * f);
+}
