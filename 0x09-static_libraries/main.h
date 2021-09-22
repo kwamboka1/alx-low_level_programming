6bdfed79c8530030208515091a9170e3790db282
kwamboka1 <makorimildred@gmail.com>
2021-09-22 01:02:09 -0700

    Modify main.h

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,3 +1,4 @@
+
 #ifndef MAIN_H
 #define MAIN_H
 
@@ -16,7 +17,6 @@ char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 char *_memset(char *s, char b, unsigned int n);
-char *_memcpy(char *dest, char *sr);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
