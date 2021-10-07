67ce1db9fab79038adb7dba5d3be1f2aa4fa3310
kwamboka1 <makorimildred@gmail.com>
2021-10-07 06:30:56 -0700

    Create 4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ *free_list - frees a list
+ *@head: head of the linked list.
+ *
+ *Return: no return,
+ *
+ */
+void free_list(list_t *head)
+{
+	list_t *now;
+
+	while ((now = head) != NULL)
+	{
+		head = head->next;
+		free(now->str);
+		free(now);
+	}
+}
