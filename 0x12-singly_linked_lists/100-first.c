7e152449942dd24a90f01680afbce298db700453
kwamboka1 <makorimildred@gmail.com>
2021-10-07 07:12:10 -0700

    Fix code

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -8,6 +8,6 @@
 
 void __attribute__((constructor)) bmain()
 {
-	printf("You are beat\! and yet, you must allow,");
-	printf("\nI bore my house upon my back\!\n");
+	printf("You are beat! and yet, you must allow,");
+	printf("\nI bore my house upon my back!\n");
 }
