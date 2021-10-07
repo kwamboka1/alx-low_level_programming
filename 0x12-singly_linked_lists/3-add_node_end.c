3264d1c8b5ad2af71b1555e0f36971e558b53058
kwamboka1 <makorimildred@gmail.com>
2021-10-07 13:23:43 -0700

    Modify 3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,28 +1,28 @@
 #include "lists.h"
 
-
 /**
- *add_node_end - adds a new node at the end of a list_t list.
- *@head: head of the linked list.
- *@str: string to store in the list
- *
- *Return: address of the head.
- *
+ * add_node_end - adds a new node at the end
+ * of a list_t list.
+ * @head: head of the linked list.
+ * @str: string to store in the list.
+ * Return: address of the head.
  */
+
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new, *temp;
-	size_t new_char;
+	size_t nchar;
 
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
+
 	new->str = strdup(str);
 
-	for (new_char = 0; str[new_char]; new_char++)
+	for (nchar = 0; str[nchar]; nchar++)
 		;
 
-	new->len = new_char;
+	new->len = nchar;
 	new->next = NULL;
 	temp = *head;
 
@@ -36,6 +36,6 @@ list_t *add_node_end(list_t **head, const char *str)
 			temp = temp->next;
 		temp->next = new;
 	}
-	return (*head);
 
+	return (*head);
 }
