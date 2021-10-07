8acfe9ea7ea66e36c8be34b36fec6f5d85ff5887
kwamboka1 <makorimildred@gmail.com>
2021-10-07 06:59:30 -0700

    Create 100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *bmain - the function to be executed before main
+ *
+ *Return: no return.
+ */
+
+void __attribute__((constructor)) bmain()
+{
+	printf("You are beat\! and yet, you must allow,");
+	printf("\nI bore my house upon my back\!\n");
+}
