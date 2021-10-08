50c38e79974bef95b7e57f0586ec65de1031ad1e
kwamboka1 <makorimildred@gmail.com>
2021-10-08 14:36:58 +0300

    Update 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -8,6 +8,6 @@
 
 void __attribute__((constructor)) before_main()
 {
-	printf("You are beat! and yet, you must allow,");
-	printf("\nI bore my house upon my back!\n");
+	printf("You are beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
