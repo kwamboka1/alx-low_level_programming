96b2503e8f1d9934c002f9691d57decc48af27ac
kwamboka1 <makorimildred@gmail.com>
2021-10-11 23:01:14 -0700

    Fix code

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,9 +1,9 @@
 #include "lists.h"
 
 /**
- *add_nodeint - add a new node at the begiinning of a linked list
+ *add_nodeint - add a new node at the beginning of a linked list
  *@head: head of a list.
- *@n: n emlement.
+ *@n: n element.
  *
  *Return: n element.
  */
