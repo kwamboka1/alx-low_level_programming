a69cf9a6ce27712fdfb5576398b87680db1d5084
kwamboka1 <makorimildred@gmail.com>
2021-10-15 02:51:12 -0700

    Fix codes

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- *binary_to uint - converts a binary number to an unsigned int.
+ *binary_to_uint - converts a binary number to an unsigned int.
  *@b: binary.
  *
  *Return: unsigned int.
  *
- */
+ **/
 
 unsigned int binary_to_uint(const char *b)
 {
