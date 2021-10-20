738a40d4989aa4947ede2efd95e1bd75223f67f7
kwamboka1 <makorimildred@gmail.com>
2021-10-20 23:06:40 +0300

    Update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -34,7 +34,7 @@ void error_file(int file_from, int file_to, char *argv[])
 int main(int argc, char *argv[])
 {
 	int file_from, file_to, err_close;
-	ssize_t nchars, nwr;
+	ssize_t chars, wr;
 	char buf[1024];
 
 	if (argc != 3)
@@ -47,14 +47,14 @@ int main(int argc, char *argv[])
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
 	error_file(file_from, file_to, argv);
 
-	nchars = 1024;
-	while (nchars == 1024)
+	chars = 1024;
+	while (chars == 1024)
 	{
-		nchars = read(file_from, buf, 1024);
-		if (nchars == -1)
+		chars = read(file_from, buf, 1024);
+		if (chars == -1)
 			error_file(-1, 0, argv);
-		nwr = write(file_to, buf, nchars);
-		if (nwr == -1)
+		wr = write(file_to, buf, chars);
+		if (wr == -1)
 			error_file(0, -1, argv);
 	}
 
