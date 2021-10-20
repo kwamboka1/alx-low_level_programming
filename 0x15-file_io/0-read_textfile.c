53e03ef746adfb343669c7c3a43f37d7ae70d01e
kwamboka1 <makorimildred@gmail.com>
2021-10-20 22:45:37 +0300

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,7 +11,7 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	ssize_t nrd, nwr;
+	ssize_t rd, wr;
 	char *buf;
 
 	if (!filename)
@@ -26,12 +26,12 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (!buf)
 		return (0);
 
-	nrd = read(fd, buf, letters);
-	nwr = write(STDOUT_FILENO, buf, nrd);
+	rd = read(fd, buf, letters);
+	wr = write(STDOUT_FILENO, buf, rd);
 
 	close(fd);
 
 	free(buf);
 
-	return (nwr);
+	return (wr);
 }
