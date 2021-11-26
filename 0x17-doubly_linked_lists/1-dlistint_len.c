970d99b9cebef6a8c6542035174ebf4e76642902
kwamboka1 <makorimildred@gmail.com>
2021-11-26 19:52:14 +0000

    Adding 1-dlistint_len.c

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * dlistint_len - returns the number of elements in
+ * a double linked list
+ *
+ * @h: head of the list
+ * Return: the number of nodes
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t number_nodes = 0;
+
+	while(h)
+	{
+	 h = h->next;
+	 number_nodes++;
+	}
+	return(number_nodes);
+
+}
