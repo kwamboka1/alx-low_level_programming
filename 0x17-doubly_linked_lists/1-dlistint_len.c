357c8eb3d6e3bee82fe0de85f3b343e32760fe82
kwamboka1 <makorimildred@gmail.com>
2021-11-26 20:16:50 +0000

    Fix Identetion error

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -5,15 +5,15 @@
  *
  * @h: head of the list
  * Return: the number of nodes
- */
+ **/
 size_t dlistint_len(const dlistint_t *h)
 {
 	size_t number_nodes = 0;
 
 	while (h)
 	{
-	 h = h->next;
-	 number_nodes++;
+		h = h->next;
+		number_nodes++;
 	}
 	return (number_nodes);
 
