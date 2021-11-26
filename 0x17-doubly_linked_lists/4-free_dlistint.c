c5109fc20273f3b2a6a2d706e43ce078b95f42dc
kwamboka1 <makorimildred@gmail.com>
2021-11-26 19:54:33 +0000

    Adding 4-free_dlistint.c

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+#include "lists.h"
+
+/**
+ * free_dlistint - Frees a linked list.
+ *
+ * @head: First node.
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
