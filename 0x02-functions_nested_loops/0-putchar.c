0309eed20eb5726bfef9a6bf01c35a91cb219be3
kwamboka1 <makorimildred@gmail.com>
2022-03-20 07:56:06 +0000

    [Modify]: The code

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,18 +6,16 @@
   * Return: Always 0
   */
 int main(void)
-
 {
-	char ch[8] = "_putchar";
-	int i;
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-	for (i = 0; i <= 8; i++)
-
-	{
-		_putchar(ch[i]);
-
-		_putchar('\n');
-
-		return(0);
-	}
+	return (0);
 }
