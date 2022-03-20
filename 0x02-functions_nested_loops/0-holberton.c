8f9985755da009e5e564c89da0c9479b94c312e6
kwamboka1 <makorimildred@gmail.com>
2022-03-20 07:39:13 +0000

    [Modify]: The file

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,23 +1,20 @@
-#include"main.h"
+#include "holberton.h"
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-
+  * main - Entry point
+  *
+  * Return: Always 0
+  */
 int main(void)
-
 {
-	char ch[8] = "_putchar";
-	int i;
-
-	for (i = 0; i <= 8; i++)
-
-	{
-		_putchar(ch[i]);
-
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 
 	return (0);
