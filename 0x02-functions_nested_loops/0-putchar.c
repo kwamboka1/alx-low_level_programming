933adbde2b8792519c14651824bfa1f9810a1c9e
kwamboka1 <makorimildred@gmail.com>
2022-03-20 07:54:18 +0000

    [Modify]: The Putchar file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+  * main - Entry point
+  *
+  * Return: Always 0
+  */
+int main(void)
+
+{
+	char ch[8] = "_putchar";
+	int i;
+
+	for (i = 0; i <= 8; i++)
+
+	{
+		_putchar(ch[i]);
+
+		_putchar('\n');
+
+		return(0);
+	}
+}
