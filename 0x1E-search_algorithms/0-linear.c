8ef13b2dae83e98779f7fc2b65d7b4e0bf8b327e
kwamboka1 <makorimildred@gmail.com>
2022-03-30 14:54:47 +0000

    [Fix]:Trailling space

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,8 +1,8 @@
 #include "search_algos.h"
 
 /**
- * linear_search - searches for a value in an array of integers using the Linear search algorithm
- *
+ * linear_search - searches for a value in an array of integers using the
+ * Linear search algorithm
  * @array: input array
  * @size: size of the array
  * @value: value to search for
